5c6f89585d3397909829c97c57eb7ba4f6760442
Sammer55 <sammerduarte@hotmail.com>
2021-07-16 09:41:14 -0300

    Added component Title

---
+++ src/components/Title/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Title = styled.text`
+    color: #000;
+    font-size: 1.2rem;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+`
+
+export default Title;
