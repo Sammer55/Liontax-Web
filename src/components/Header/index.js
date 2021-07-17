947811e2ecd899389e7d2d097125b04cc638cbe1
Sammer55 <sammerduarte@hotmail.com>
2021-07-17 09:07:54 -0300

    Added Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,18 +3,19 @@ import styled from 'styled-components';
 import lion from '../../assets/lion.png';
 
 const Header = styled.div`
-    background-color: #000;
+    background-color: #FFF;
     position: relative;
     display: flex;
     align-items: center;
     width: 100%;
     height: 80px;
     padding-left: 2rem;
+    border-bottom: 1.2px solid #C5C5C5;
 `
 
 const NavItem = styled.div`
-    font-size: 20px;
-    color: #FFF;
+    font-size: 25px;
+    color: #000;
 `
 
 export default function header() {
