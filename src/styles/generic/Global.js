a2da04b5f871103ebd062e6e19004b1511a9caf1
Sammer55 <sammerduarte@hotmail.com>
2021-07-17 09:04:39 -0300

    Added background-color to Global Style

--- src/styles/generic/Global.js
+++ src/styles/generic/Global.js
@@ -4,6 +4,7 @@ const Global = createGlobalStyle`
     body {
         margin: 0;
         padding: 0;
+        background-color: #FD6F01;
         font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; 
     }
 `
