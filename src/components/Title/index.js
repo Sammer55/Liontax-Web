f0ac162b60b3ec7dd6117e94f0a08e3f49d7f778
Sammer55 <sammerduarte@hotmail.com>
2021-07-17 09:08:11 -0300

    Added Title component

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import styled from 'styled-components';
 
 const Title = styled.text`
     color: #000;
-    font-size: 1.2rem;
+    font-size: 1.3rem;
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-weight: bold;
 `
 
-export default Title;
+export default function title({ text }) {
+
+    return(
+        <Title>
+            { text }
+        </Title>
+    )
+}
