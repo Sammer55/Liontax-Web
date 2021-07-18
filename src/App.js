e5089303ee3f202fa17112b4beb84a9d9fb9f43c
Sammer55 <sammerduarte@hotmail.com>
2021-07-18 08:19:25 -0300

    Pass Router, Switch and Routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import Header from './components/Header';
-import Global from './styles/generic/Global';
-// import Content from './components/Users/Create';
-import Search from './components/Users/Search';
+import React from "react";
+import Header from "./components/Header";
+import Global from "./styles/generic/Global";
+import Create from "./pages/clients/create";
+import Search from "./pages/clients/search";
+import Edit from "./pages/clients/edit";
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <React.Fragment>
-      <Global />
+    <Router>
       <Header />
-      <Search />
-    </React.Fragment>
+      <Switch>
+        <Route path="/" exact component={Search} />
+        <Route path="/create" component={Create} />
+        <Route path="/edit" component={Edit} />
+      </Switch>
+      <Global />
+    </Router>
   );
 }
 
