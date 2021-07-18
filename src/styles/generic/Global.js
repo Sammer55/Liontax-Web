b2be2f832ffdfc86d3b74606220f89c378fa9099
Sammer55 <sammerduarte@hotmail.com>
2021-07-18 08:15:49 -0300

    Edited Background-color

--- src/styles/generic/Global.js
+++ src/styles/generic/Global.js
@@ -1,11 +1,11 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 const Global = createGlobalStyle`
     body {
         margin: 0;
         padding: 0;
-        background-color: #FD6F01;
-        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; 
+        background-color: #F1F1F1;
+        font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif; 
     }
 `
 
