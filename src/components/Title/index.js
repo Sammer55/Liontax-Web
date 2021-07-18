c802ff758dc769bb4c4d11463a7ec8bf5dfce09c
Sammer55 <sammerduarte@hotmail.com>
2021-07-18 08:17:31 -0300

    Edited component Title

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const Title = styled.text`
-    color: #000;
-    font-size: 1.3rem;
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    font-weight: bold;
+const StyledTitle = styled.h1`
+    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
 `
 
-export default function title({ text }) {
+export default function Title({ text }) {
 
     return(
-        <Title>
+        <StyledTitle>
             { text }
-        </Title>
+        </StyledTitle>
     )
 }
