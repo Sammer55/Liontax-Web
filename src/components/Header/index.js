7dd7b14f9f7bb651970e873dea86a27021a75ac9
Sammer55 <sammerduarte@hotmail.com>
2021-07-19 15:31:52 -0300

    Removed navitems from header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,52 +10,21 @@ const Header = styled.div`
     align-items: center;
     width: 100%;
     height: 80px;
-    padding-left: 2rem;
     border-bottom: 3px solid #C5C5C5;
-`
-
-const NavTitle = styled.div`
-    font-size: 25px;
-    color: #000;
-`
-
-const NavItem = styled.div`
-    font-size: 20px;
-    color: #000;
-    background-color: #FFF;
-    &:hover {
-        color: #FF6F00;
-    }
-    padding: 8px;
+    justify-content: center
 `
 
 const NavItemsContainer = styled.div`
     display: flex;
-    width: 70%;
-    justify-content: space-evenly;
+    align-items: center;
 `
 
 export default function header() {
-
     return (
         <Header>
-            <Link to="/" style={{ textDecoration: 'none' }}>
-                <NavTitle>
-                    <img src={lion} width="90" height="75" alt="logo" />
-                </NavTitle>
-            </Link>
-
-            <Link to="/" style={{ textDecoration: 'none' }}>
-                <NavTitle>Lion Tax</NavTitle>
-            </Link>
-
             <NavItemsContainer>
                 <Link to="/" style={{ textDecoration: 'none' }}>
-                    <NavItem>Lista de clientes</NavItem>
-                </Link>
-
-                <Link to="/create" style={{ textDecoration: 'none' }}>
-                    <NavItem>Adicionar cliente</NavItem>
+                    <img src={lion} width="90" height="75" alt="lion" />
                 </Link>
             </NavItemsContainer>
         </Header>
