60361e5d63e27ff9e472aa134ab15ad45a9226b7
Sammer55 <sammerduarte@hotmail.com>
2021-07-20 11:55:22 -0300

    Edited Tel format

--- src/components/TextFormat/formatTel/index.js
+++ src/components/TextFormat/formatTel/index.js
@@ -1,9 +1,10 @@
 export const formatTel = (tel) => {
     const telToString = tel.toString()
-    const DDD = telToString.substr(1, 2)
-    const firstsNumbers = telToString.substr(5, 5)
-    const lastNumbers = telToString.substr(11, 4)
-    const telFormated = '+55' + DDD + firstsNumbers + lastNumbers
+    const DDD = telToString.substr(0, 3)
+    const firstsNumbers = telToString.substr(5, 2)
+    const lastNumbers = telToString.substr(9, 5)
+    const latestNumbers = telToString.substr(15, 4)
+    const telFormated = DDD + firstsNumbers + lastNumbers + latestNumbers
     return telFormated;
 
 }
