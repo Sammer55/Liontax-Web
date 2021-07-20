fb6686c9e30427e693505bbd0867afd837ee8229
Sammer55 <sammerduarte@hotmail.com>
2021-07-20 11:55:32 -0300

    Edited CPF format

--- src/components/TextFormat/formatCpf/index.js
+++ src/components/TextFormat/formatCpf/index.js
@@ -1,9 +1,9 @@
 export const formatCpf = (cpf) => {
-    const firstBlock = cpf.substr(0, 3)
-    const secondBlock = cpf.substr(4, 3)
-    const thirdBlock = cpf.substr(8, 3)
-    const fourBlock = cpf.substr(12, 2)
+    const cpfToString = cpf.toString()
+    const firstBlock = cpfToString.substr(0, 3)
+    const secondBlock = cpfToString.substr(4, 3)
+    const thirdBlock = cpfToString.substr(8, 3)
+    const fourBlock = cpfToString.substr(12, 2)
     const cpfFormated = firstBlock + secondBlock + thirdBlock + fourBlock
     return cpfFormated;
-
 }
